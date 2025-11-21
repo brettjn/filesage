675522fa010c4fce7af8438f66b177dc9bf953b9
brettjn <brettjnelson@protonmail.com>
2025-11-21 11:38:40 -0700

    New gitignore, missing lib.

---
+++ lib/ANNOUNCE_fscan.py
@@ -0,0 +1,8 @@
+
+# License text shown in About dialog detailed view
+ANNOUNCE_TEXT = """
+fileSage  Copyright (C) 2025  Brett J. Nelson
+This program comes with ABSOLUTELY NO WARRANTY;
+This is free software, and you are welcome to redistribute it
+under certain conditions; use --licence for details.
+"""
